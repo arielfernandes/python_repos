3bacd67a8681b48a6bea91345b309526e2c5cf4b
arielfernandes3 <0.arielfernandes.0@gmail.com>
2022-01-06 09:08:26 -0300

    add url link

--- python_repo.py
+++ python_repo.py
@@ -14,18 +14,23 @@ print("Total repositorios: ", response_dic['total_count'])
 # Explora informações sobre os repositórios
 repo_dicts = response_dic['items']
 
-name, stars = [], []
+name, plot_dicts = [], []
 
 for repo_dict in repo_dicts:
     name.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': repo_dict['description'],
+        'xlink': repo_dict['html_url']
+    }
+    plot_dicts.append(plot_dict)
 # cria a visualização
 my_style = LS('#333366', base_style=LCS)
 my_config = pygal.Config()
 my_config.x_label_rotation = 45
 my_config.show_legend = False
 my_config.title_font_size = 24
-my_config.major_label_font_size = 18
+my_config.major_label_font_size = 10
 my_config.truncate_label = 15
 my_config.show_y_guides = False
 my_config.width = 1000
@@ -36,5 +41,5 @@ chart = pygal.Bar(my_config, style=my_style)
 chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = name
-chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('python_repos.svg')
